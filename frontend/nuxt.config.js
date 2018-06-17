98eae084ca3b21b46e50498515f74daff48d2a1e
jehyuung.park <jehyung4963@naver.com>
2018-06-17 20:04:34 +0900

    3

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'NAVER CLOUD PLATFORM',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'http://cdn.jsdelivr.net/font-nanum/1.0/nanumbarungothic/nanumbarungothic.css' }
+    ]
+  },
+  loading: {
+    color: '#14adea'
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    {src: '~/assets/less/app.less', lang: 'less'}
+  ],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  serverMiddleware: [
+    // API middleware
+    '~/api/index.js'
+  ]
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section id="main" class="container">
+
+    메인 페이지입니다.
+
+  </section>
+</template>
